0222f6e4baccd468d9b7747ecd30d596bd4fbce8
Matthew Cowell <matt@cowebtech.ca>
2021-05-05 15:40:46 -0400

    Revert "Update addon.xml to include the new python version"
    This reverts commit aa45d9e41ec5a3830c15a3f9a6843212b8f25d51.

--- plugin.video.itv/service.py
+++ plugin.video.itv/service.py
@@ -3,10 +3,14 @@ import os,xbmc,xbmcaddon
 PLUGIN='plugin.video.itv'
 ADDON = xbmcaddon.Addon(id=PLUGIN)
 
+
 if ADDON.getSetting('delcache')=='true':
     cache = xbmc.translatePath(os.path.join('special://temp'))
 
-    for root, dirs, files in os.walk(cache):           
+    for root, dirs, files in os.walk(cache):
+
+
+                    
         for f in files:
             if not '.log' in f:
                 try:os.unlink(os.path.join(root, f))
